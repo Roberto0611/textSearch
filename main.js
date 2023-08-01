d3e7cd49f8c60b8f81dcd28667ac1aa46160a388
Roberto0611 <ochoacuevasr@gmail.com>
2023-08-01 16:56:09 -0700

    change to the css and starting js

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 //Script by: Roberto Ochoa Cuevas :).
 
+//get the text content
+const text = document.getElementById('sText').textContent;
+
+//search for the words
+const test = text.match(/Lorem/ig)
+console.log(test)
+alert('we found' + ' ' + test.length + ' ' + 'elements')
