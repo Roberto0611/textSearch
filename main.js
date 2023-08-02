29ac9e21f1aa8b0ca241cfd613a9b4e83df53ea0
Roberto0611 <ochoacuevasr@gmail.com>
2023-08-01 19:03:12 -0700

    finished proyect :)

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 //Script by: Roberto Ochoa Cuevas :).
+let word;
 
-//get the text content
+//get the text and text content
+const textElement = document.getElementById('sText');
 const text = document.getElementById('sText').textContent;
 
-//search for the words
-const test = text.match(/Lorem/ig)
-console.log(test)
-alert('we found' + ' ' + test.length + ' ' + 'elements')
+//Event listener for the button
+const button = document.getElementById('search-btn');
+button.addEventListener('click', () =>{
+    //get the word
+    word = document.getElementById('keyword-input').value
+
+    //search for the matches
+    const regEx = new RegExp(word, "ig");
+    const matches = text.match(regEx);
+
+    //Higligth the words
+    if (matches) {
+        const highlightedText = text.replace(regEx, (match) => `<span class="highlight">${match}</span>`);
+        textElement.innerHTML = highlightedText;
+        alert('We found ' + matches.length + ' occurrences.');
+    } else {
+        alert('No matches found.');
+    }
+})
+//<33
